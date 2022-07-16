db141897a772c2e7efb010cf1477261fa18e069d
NMPhap <21522441@gm.uit.edu.vn>
2022-07-16 15:39:29 +0700

    delete

--- FoodDeliveryApp/source/constant/font.js
+++
@@ -1,6 +0,0 @@
-const CUSTOM_FONT = {
-  SF_PRO_ROUNDED: 'FontsFree-Net-SF-Pro-Rounded-Regular',
-  SF_PRO_ROUNDED_BOLD: 'FontsFree-Net-SF-Pro-Rounded-Bold',
-};
-
-export default CUSTOM_FONT;
--- FoodDeliveryApp/source/image/index.js
+++
@@ -1,2 +0,0 @@
-export {default as IMG_Logo} from './logo.png'
-export {default as IMG_background} from './background.png'
