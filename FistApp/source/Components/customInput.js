cda16b0edf478d9bfc4773fc013c9d9ef6ab2afe
NMPhap <21522441@gm.uit.edu.vn>
2022-07-18 22:56:05 +0700

    update: delete unnecessary code, add getChangText

--- FistApp/source/Components/customInput.js
+++ FistApp/source/Components/customInput.js
@@ -1,5 +1,5 @@
 import React, {Component } from "react";
-import {Text, StyleSheet,TextInput, _Text} from 'react-native';
+import {Text, StyleSheet, View, SafeAreaView, TextInput, Button, Image, TouchableOpacity, NativeEventEmitter, _Text} from 'react-native';
 import CUSTOM_COLOR from "../constant/color";
 export class CustomInput extends Component{
     constructor(props){
@@ -10,18 +10,21 @@ export class CustomInput extends Component{
             secureTextEntry: props.secureTextEntry | false,
         };
     }
+
+    getChangeText = text =>{
+        return (this.state.value.length < text.length)? this.state.value + text.charAt(text.length - 1): this.state.value.substring(0,this.state.value.length - 1);
+    }
     render() {
-        console.log('this.state', this.state);
         return(
         <>
             <Text style = {styles.inputLabel}>{this.state.label}</Text>
             <TextInput
             style={styles.inputContainer}
             onChangeText={text => {
-                this.setState({value: (this.state.value.length < text.length)? this.state.value + text.charAt(text.length - 1): this.state.value.substring(0,this.state.value.length - 1)});
+                this.setState({value: this.getChangeText(text)});
             }}
             placeholder = {this.props.placeHolder}
-            placeholderTextColor= {CUSTOM_COLOR.WhiteSmoke}
+            placeholderTextColor= {CUSTOM_COLOR.Whisper}
             value = {(this.props.secureTextEntry == 1)? this.props.secureTextEntryText.repeat(this.state.value.length):this.state.value}
             />
         </>
@@ -32,14 +35,13 @@ export class CustomInput extends Component{
 const styles = StyleSheet.create({
     inputContainer: {
         height: 40,
-        color: 'black',
-        borderBottomColor: 'black',
+        color: CUSTOM_COLOR.Black,
+        borderBottomColor: CUSTOM_COLOR.Black,
         borderBottomWidth: 1,
-        co
     },
   
     inputLabel: {
-    color: 'black',
+    color: CUSTOM_COLOR.Black,
     opacity: 0.4,
     },
 })
