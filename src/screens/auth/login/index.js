3b31ce7682f1463d4d9a85cdf9ed7f3b601ef436
NMPhap <21522441@gm.uit.edu.vn>
2022-07-21 12:02:11 +0700

    no message

--- src/screens/auth/login/index.js
+++ src/screens/auth/login/index.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, SafeAreaView, Image, Text} from 'react-native';
+import {View, SafeAreaView, Image, Text} from 'react-native';
 import SwitchButton from '../../../components/SwitchButton';
 import scale from '../../../constants/responsive';
 import {IMG_bella, IMG_vector1} from '../../../assets/images';
 import Form from '../forms';
 import CUSTOM_COLOR from '../../../constants/colors';
 import UnderlineButton from '../../../components/UnderlineButton';
-import CUSTOM_FONT from '../../../constants/fonts';
+import styles from './styles';
 
 export default class LoginScreen extends Component {
   constructor(props) {
@@ -50,7 +50,6 @@ export default class LoginScreen extends Component {
             flexDirection={'row'}>
             <View style={styles.buttonContainer}>
               <UnderlineButton
-                label={'Login'}
                 onPress={() => this.setState({label: 'Login'})}
                 underlineStyle={[
                   styles.underline,
@@ -64,7 +63,6 @@ export default class LoginScreen extends Component {
                 <Text style={styles.buttonText}>{'Login'}</Text>
               </UnderlineButton>
               <UnderlineButton
-                label={'Sign-Up'}
                 onPress={() => this.setState({label: 'SignUp'})}
                 underlineStyle={[
                   styles.underline,
@@ -85,61 +83,3 @@ export default class LoginScreen extends Component {
     );
   }
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    borderRadius: 30,
-  },
-
-  upper: {
-    justifyContent: 'center',
-    alignContent: 'center',
-    width: '100%',
-    height: scale(382),
-    elevation: 20,
-    backgroundColor: 'white',
-    borderRadius: 30,
-    top: -20,
-    overflow: 'hidden',
-  },
-  logoContainer: {
-    justifyContent: 'center',
-    alignContent: 'center',
-  },
-  logo: {
-    width: scale(131.53),
-    height: scale(162.38),
-    justifyContent: 'center',
-    alignSelf: 'center',
-    resizeMode: 'cover',
-  },
-  buttonContainer: {
-    paddingTop: 10,
-    width: '100%',
-    flexDirection: 'row',
-    bottom: 0,
-    position: 'absolute',
-    overflow: 'hidden',
-    justifyContent: 'space-evenly',
-  },
-  button: {
-    flex: 1,
-    position:'absolute',
-    bottom: 10,
-  },
-  buttonText: {
-    color: 'black',
-    fontFamily: CUSTOM_FONT.Bold,
-    alignSelf: 'center',
-    justifyContent: 'center',
-    fontSize: 18,
-  },
-
-  underline: {
-    width: scale(134),
-    height: 3,
-    alignSelf: 'center',
-  },
-});
