633f229647a1e56d5ecfcd8b79ccd38338158af1
Phuoc Hung <phuochunglmht@gmail.com>
2022-07-22 21:16:09 +0700

    resolve AsyncStorage warning

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,16 +1,29 @@
-import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from 'firebase/app';
+import {
+  initializeAuth,
+  getReactNativePersistence
+} from 'firebase/auth/react-native';
+
+// add firebase config here
 const firebaseConfig = {
-    apiKey: "AIzaSyCFGFxzTSVUG-tb-NgjziBs4A9YRWpaQUY",
-    authDomain: "food-for-everyone-e506f.firebaseapp.com",
-    projectId: "food-for-everyone-e506f",
-    storageBucket: "food-for-everyone-e506f.appspot.com",
-    messagingSenderId: "869719383889",
-    appId: "1:869719383889:web:6d83ffad7065e51d2dfd70",
-    measurementId: "G-VHJXBB44JV"
-  };
+  apiKey: "AIzaSyCFGFxzTSVUG-tb-NgjziBs4A9YRWpaQUY",
+  authDomain: "food-for-everyone-e506f.firebaseapp.com",
+  projectId: "food-for-everyone-e506f",
+  storageBucket: "food-for-everyone-e506f.appspot.com",
+  messagingSenderId: "869719383889",
+  appId: "1:869719383889:web:6d83ffad7065e51d2dfd70",
+  measurementId: "G-VHJXBB44JV"
+};
 
-  const app = initializeApp(firebaseConfig);
 
-  export const auth = getAuth(app);
+// initialize firebase app
+const app = initializeApp(firebaseConfig);
+
+// initialize auth
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+
+export { auth };
