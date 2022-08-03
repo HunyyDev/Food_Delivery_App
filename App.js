8e41dcd4742633cfe29b857e77c38be641d1c422
NMPhap <21522441@gm.uit.edu.vn>
2022-08-03 09:35:04 +0700

    <dev>: add custom slider

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import LoginScreen from './src/screens/auth/login';
 import {Onboarding} from './src/screens/onboarding';
-import React, {Component, useContext, useState} from 'react';
+import React, { useContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {InProgress} from './src/screens/inProgress';
 import History from './src/screens/history';
-import Orders from './src/screens/orders';
 import Loading from './src/screens/Loading';
 import { AuthContext } from './src/contexts/AuthContext';
 import {createStackNavigator} from '@react-navigation/stack';
 import MyDrawer from './src/screens/drawer';
-import CustomScrollDrawer from './src/screens/drawer';
 
 const Stack = createStackNavigator();
 const App = props => {
